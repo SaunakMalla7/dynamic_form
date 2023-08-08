ea230a4c71a75e85412f99e36fc65e03f4331c35
SaunakMalaSaunakMalla7 <SaunakMalaSaunakMalla7@gmail.com>
2023-08-08 16:09:35 +0545

    commit 2

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import './App.css';
 
 const data = {
   form: {
@@ -7,39 +8,105 @@ const data = {
         name: "name",
         label: "Name",
         required: true,
-        data_type: "Integer",
-        html_element: "textbox",
+        data_type: "String",
+        html_element: "text",
       },
       {
         name: "email",
         label: "Email",
-        hidden: false,
         required: true,
         data_type: "String",
         html_element: "email",
+      },
+      {
+        name: "age",
+        label: "Age",
+        required: true,
+        data_type: "Integer",
+        html_element: "number",
+      },
+      {
+        name: "role",
+        label: "Role",
+        required: true,
+        data_type: "String",
+        html_element: "select",
+        options: [
+          { label: "Admin", value: "admin" },
+          { label: "User", value: "user" },
+        ],
+      },
+      {
+        name: "Condition",
+        label: "Do You Accept All The Condition",
+        required: false,
+        data_type: "Boolean",
+        html_element: "checkbox",
       },
     ],
   },
 };
 
+
+
 function Form() {
+  const [formData, setFormData] = useState({});
+  const [formErrors, setFormErrors] = useState({});
+
+
+  const handleInputChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    const fieldValue = type === "checkbox" ? checked : value;
+
+    setFormData((prevData) => ({ ...prevData, [name]: fieldValue }));
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted Data:", formData );
+  };
   return (
-    <div>
-      <form>
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
         {data.form.fields.map((inputData, index) => {
-          console.log("inputData", inputData);
+          // console.log("inputData", inputData);
           return (
             <div key={index}>
               <label>{inputData.label}</label>
-              <input
-                type={inputData.html_element}
-                name={inputData.name}
-                required={inputData.required}
-                datatype={inputData.data_type}
-              />
+              {inputData.html_element === "select" ? (
+                <select
+                className="form-select"
+                  name={inputData.name}
+                  required={inputData.required}
+                  datatype={inputData.data_type}
+                  onChange={handleInputChange}
+                >
+                  <option value="">Select an option</option>
+                  {inputData.options.map((option, optionIndex) => (
+                    <option key={optionIndex} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              ) 
+              :  (
+                <input
+                className="form-input"
+                  type={inputData.html_element}
+                  name={inputData.name}
+                  required={inputData.required}
+                  datatype={inputData.data_type}
+                  onChange={handleInputChange}
+                />
+              )}
+                {formErrors[inputData.name] && (
+              <div className="error-message">{formErrors[inputData.name]}</div>
+            )}
             </div>
           );
         })}
+        <button className="form-button" type="submit">Submit</button>
       </form>
     </div>
   );
