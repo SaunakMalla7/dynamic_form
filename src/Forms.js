0551d683c4d9ceb4d0471450b7c97cdf0121e1e0
SaunakMalaSaunakMalla7 <SaunakMalaSaunakMalla7@gmail.com>
2023-08-09 21:02:23 +0545

    max min from schema

--- src/Forms.js
+++ src/Forms.js
@@ -1,5 +1,5 @@
 // import React, { useState } from "react";
-// import './App.css';
+// import "./App.css";
 
 // const data = {
 //   form: {
@@ -10,6 +10,8 @@
 //         required: true,
 //         data_type: "String",
 //         html_element: "text",
+//         minLength: 5,
+//         maxLength: 30,
 //       },
 //       {
 //         name: "email",
@@ -24,7 +26,17 @@
 //         required: true,
 //         data_type: "Integer",
 //         html_element: "number",
+//         minLength: 18,
+//         maxLength: 100,
 //       },
+//       {
+//         name: "number",
+//         label: "Number",
+//         required: true,
+//         data_type: "Integer",
+//         html_element: "number",
+//       },
+
 //       {
 //         name: "role",
 //         label: "Role",
@@ -47,12 +59,9 @@
 //   },
 // };
 
-
-
 // function Forms() {
 //   const [formData, setFormData] = useState({});
 //   const [formErrors, setFormErrors] = useState({});
-  
 
 //   const handleInputChange = (event) => {
 //     const { name, value, type, checked } = event.target;
@@ -61,90 +70,112 @@
 //     setFormData((prevData) => ({ ...prevData, [name]: fieldValue }));
 //   };
 
-//   const validateForm = () => {
-//     const errors = {};
+//   const handleBlur = (fieldName, value) => {
+//     validateField(fieldName, value);
+//   };
 
-//     data.form.fields.forEach((inputData) => {
-//       const value = formData[inputData.name];
+//   const validateField = (fieldName, value) => {
+//     const errors = { ...formErrors };
+//     const inputData = data.form.fields.find((field) => field.name === fieldName);
 
+//     if (inputData) {
 //       if (inputData.required && !value) {
 //         errors[inputData.name] = `${inputData.label} is required`;
+//       } else {
+//         errors[inputData.name] = undefined;
 //       }
 
-//       if (inputData.data_type === "Integer") {
+//       if (inputData.data_type === "Integer") { // Adjusted property name
 //         const numValue = parseInt(value, 10);
-
 //         if (isNaN(numValue)) {
 //           errors[inputData.name] = `${inputData.label} must be a valid number`;
-//         } else {
-//           if (numValue < 18) {
-//             errors[inputData.name] = `${inputData.label} should be at least 18`;
-//           }
-
-//           if (numValue > 100) {
-//             errors[inputData.name] = `${inputData.label} should not exceed 100`;
-//           }
+//         } else if (numValue < inputData.minLength) { // Use inputData.minLength
+//           errors[inputData.name] = `${inputData.label} should be at least ${inputData.minLength}`;
+//         } else if (numValue > inputData.maxLength) { // Use inputData.maxLength
+//           errors[inputData.name] = `${inputData.label} should not exceed ${inputData.maxLength}`;
 //         }
 //       }
-//     });
+
+//       if (inputData.data_type === "String") { // Adjusted property name
+//         if (value.length < inputData.minLength || value.length > inputData.maxLength) {
+//           errors[inputData.name] = `${inputData.label} must be between ${inputData.minLength} and ${inputData.maxLength} characters`;
+//         }
+//       }
+//     }
 
 //     setFormErrors(errors);
-//     return Object.keys(errors).length === 0;
 //   };
 
 //   const handleSubmit = (event) => {
 //     event.preventDefault();
 
-//     if (validateForm()) {
+//     const ageValue = parseInt(formData.age, 10);
+//     const nameValue = formData.name;
+
+//     if (validateForm() && ageValue >= 18 && ageValue <= 100 && nameValue.length >= 5 && nameValue.length <= 30) {
 //       console.log("Submitted Data:", formData);
 //     } else {
-//       console.log("Form contains errors");
+//       console.log("Form contains errors ");
 //     }
+//   };
+
+//   const validateForm = () => {
+//     const errors = {};
+
+//     data.form.fields.forEach((inputData) => {
+//       const value = formData[inputData.name];
+//       validateField(inputData.name, value);
+//       if (errors[inputData.name]) {
+//         return false;
+//       }
+//     });
+//     return true;
 //   };
 
 //   return (
 //     <div className="form-container">
 //       <form onSubmit={handleSubmit}>
-//         {data.form.fields.map((inputData, index) => {
-//           // console.log("inputData", inputData);
-//           return (
-//             <div key={index}>
-//               <label>{inputData.label}</label>
-//               {inputData.html_element === "select" ? (
-//                 <select
+//         {data.form.fields.map((inputData, index) => (
+//           <div key={index}>
+//             <label>{inputData.label}</label>
+//             {inputData.html_element === "select" ? (
+//               <select
 //                 className="form-select"
-//                   name={inputData.name}
-//                   required={inputData.required}
-//                   datatype={inputData.data_type}
-//                   onChange={handleInputChange}
-//                 >
-//                   <option value="">Select an option</option>
-//                   {inputData.options.map((option, optionIndex) => (
-//                     <option key={optionIndex} value={option.value}>
-//                       {option.label}
-//                     </option>
-//                   ))}
-//                 </select>
-//               ) 
-//               :  (
-//                 <input
+//                 name={inputData.name}
+//                 required={inputData.required}
+//                 datatype={inputData.data_type}
+//                 onChange={handleInputChange}
+//                 onBlur={(e) => handleBlur(inputData.name, e.target.value)}
+//               >
+//                 <option value="">Select an option</option>
+//                 {inputData.options.map((option, optionIndex) => (
+//                   <option key={optionIndex} value={option.value}>
+//                     {option.label}
+//                   </option>
+//                 ))}
+//               </select>
+//             ) : (
+//               <input
 //                 className="form-input"
-//                   type={inputData.html_element}
-//                   name={inputData.name}
-//                   required={inputData.required}
-//                   datatype={inputData.data_type}
-//                   onChange={handleInputChange}
-//                 />
-//               )}
-//                 {formErrors[inputData.name] && (
+//                 type={inputData.html_element}
+//                 name={inputData.name}
+//                 required={inputData.required}
+//                 datatype={inputData.data_type}
+//                 onChange={handleInputChange}
+//                 onBlur={(e) => handleBlur(inputData.name, e.target.value)}
+//               />
+//             )}
+//             {formErrors[inputData.name] && (
 //               <div className="error-message">{formErrors[inputData.name]}</div>
 //             )}
-//             </div>
-//           );
-//         })}
-//         <button className="form-button" type="submit">Submit</button>
+//           </div>
+//         ))}
+//         <button className="form-button" type="submit">
+//           Submit
+//         </button>
 //       </form>
 //     </div>
 //   );
 // }
+
 // export default Forms;
